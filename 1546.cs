6f8d8e687c165589f8249baded4c94d5a3a55415
MurRuck <53210642+MurRuck@users.noreply.github.com>
2023-11-08 22:19:34 +0900

    Add files via upload

---
+++ 1546.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            string[] arr = Console.ReadLine().Split(" ");
+            int max = 0;
+            float result = 0;
+
+            for(int i = 0; i < num; i++)
+                if (max < int.Parse(arr[i]))
+                    max = int.Parse(arr[i]);
+            
+            for(int i = 0; i < num; i++)
+                result += (float.Parse(arr[i]) / max) * 100;
+
+            Console.WriteLine(result / num);
+        }
+    }
+}
