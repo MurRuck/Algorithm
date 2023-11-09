4f2d9890eea313d907d45c427ee4220270bb69aa
MurRuck <53210642+MurRuck@users.noreply.github.com>
2023-11-09 23:18:59 +0900

    Add files via upload

---
+++ 1181.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            //string[] input = new string[num];
+            List<string> input = new List<string>();
+
+            for (int i = 0; i < num; i++)
+            {
+                input.Add(Console.ReadLine().ToLower());
+            }
+
+            input = input.Distinct().ToList();
+            input.Sort();
+            input = input.OrderBy(x => x.Length).ToList();
+            for (int i = 0; i < input.Count; i++)
+            {
+                    Console.WriteLine(input[i]);
+            }
+        }
+    }
+}
